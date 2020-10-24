b58dc82741219907e5a73e10c548cc47c5a660f5
Anish kr singh <65110035+Ankrsi@users.noreply.github.com>
2020-10-24 12:48:37 +0530

    Update urls.py

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-import views
+from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.web_page1,name='page1'),
+    path('',web_page1,name='page1'),
 ]
